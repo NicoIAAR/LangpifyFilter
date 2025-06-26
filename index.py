677d7aaf46b7826f4f0fe82cc768569a07eed48d
NicoIAAR <nicolashernanaguero@live.com>
2025-06-26 09:54:46 -0300

    Update index.py
    add ignore previus instruction and lower case

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ def limpiar_input(user_input):
     # Solo permite letras, números y espacios
     texto_limpio = re.sub(r'[^a-zA-Z0-9 ]', '', user_input)
     # Elimina palabras peligrosas
-    palabras_prohibidas = [r'DATABASE', r'base de datos', r'SQL', r'SELECT', r'UPDATE', r'GET', r'DELETE', r'POST', r'PUT', r'TABLE', r'GROUP BY', r'ORDER BY', r'INSERT', r'mysql',r'mongodb', r'mongoose', r'mongo', r'password', r'username', r'user', r'pass', r'root', r'admin', r'contraseña']
+    palabras_prohibidas = [r'DATABASE', r'base de datos', r'SQL', r'SELECT', r'UPDATE', r'GET', r'DELETE', r'POST', r'PUT', r'TABLE', r'GROUP BY', r'ORDER BY', r'INSERT', r'mysql',r'mongodb', r'mongoose', r'mongo', r'password', r'username', r'user', r'pass', r'root', r'admin', r'contraseña', r'Ignore previous instructions', r'ignorar instrucciones previas']
     for palabra in palabras_prohibidas:
         # Elimina la palabra, sin importar mayúsculas/minúsculas
         texto_limpio = re.sub(palabra, '', texto_limpio, flags=re.IGNORECASE)
@@ -12,6 +12,6 @@ def limpiar_input(user_input):
 
 if __name__ == "__main__":
     entrada = input("Introduce un texto: ")
-    limpio = limpiar_input(entrada)
+    limpio = limpiar_input(entrada).lower()
     print(f"Texto limpio: {limpio}")
 
