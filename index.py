d72cb6664892c230f9551dc718cdf2e8cc2d424c
NicoIAAR <nicolashernanaguero@live.com>
2025-06-29 18:12:42 -0300

    Update index.py
    clean code

--- index.py
+++ index.py
@@ -5,17 +5,19 @@ def limpiar_input(user_input):
     texto_limpio = re.sub(r'[^a-zA-Z0-9 áéíóúÁÉÍÓÚüÜñÑ]', '', user_input)
     # Elimina palabras peligrosas
     palabras_prohibidas = [
-        r'DATABASE', r'base de datos', r'SQL', r'SELECT', r'UPDATE', r'GET', r'DELETE', r'POST', r'PUT', r'TABLE', r'GROUP BY', r'ORDER BY', r'INSERT',
-        r'INNER JOIN', r'mysql', r'mongodb', r'mongo', r'mongod', r'mongoose', r'password', r'username', r'user', r'pass', r'root', r'admin', r'contraseña',
-        r'Ignore previous instructions', r'ignorar instrucciones previas', r'inspect', r'html', r'xss', r'iframe', r'script', r'http', r'https', r'localhost', r'hostname',
-        r'cookie', r'cookies', r'function', r'void', r'xhr', r'php', r'htmlentities', r'alert', r'xml',
-        r'drop', r'truncate', r'alter', r'create', r'grant', r'revoke', r'union', r'cast', r'char', r'varchar', r'where', r'from', r'into', r'values',
-        r'--', r'#', r';', r' or ', r' and ', r' not ', r' like ', r' between ', r'count', r'benchmark', r'sleep', r'waitfor', r'xp_', r'0x',
-        r'load_file', r'outfile', r'dumpfile', r'shutdown', r'exec', r'execute', r'xp_cmdshell', r'openrowset', r'openquery', r'sysobjects', r'syscolumns',
-        r'information_schema', r'pg_', r'pg_catalog', r'current_user', r'session_user', r'system_user', r'pg_sleep', r'pg_terminate_backend',
-        r'@@version', r'@@hostname', r'@@datadir', r'@@tmpdir', r'@@basedir', r'@@innodb_version', r'@@version_compile_os',
-        r'file_priv', r'root@', r'0=0', r'1=1', r'2=2', r'3=3', r'4=4', r'5=5', r'6=6', r'7=7', r'8=8', r'9=9'
+        'DATABASE', 'base de datos','mysql', 'SQL', 'SELECT', 'UPDATE', 'GET', 'DELETE', 'POST', 'PUT', 'TABLE', 'GROUP BY', 'ORDER BY', 'INSERT',
+        'INNER JOIN', 'mongodb', 'mongo', 'mongod', 'mongoose', 'password', 'username', 'user', 'pass', 'root', 'admin', 'contraseña', 'ignore',
+        'Ignore previous instructions', 'ignorar instrucciones previas', 'inspect', 'html', 'xss', 'iframe', 'script', 'http', 'https', 'localhost', 'hostname',
+        'cookie', 'cookies', 'function', 'void', 'xhr', 'php', 'htmlentities', 'alert', 'xml',
+        'drop', 'truncate', 'alter', 'create', 'grant', 'revoke', 'union', 'cast', 'char', 'varchar', 'where', 'from', 'into', 'values',
+        '--', '#', ';', ' or ', ' and ', ' not ', ' like ', ' between ', 'count', 'benchmark', 'sleep', 'waitfor', 'xp_', '0x',
+        'load_file', 'outfile', 'dumpfile', 'shutdown', 'exec', 'execute', 'xp_cmdshell', 'openrowset', 'openquery', 'sysobjects', 'syscolumns',
+        'information_schema', 'pg_', 'pg_catalog', 'current_user', 'session_user', 'system_user', 'pg_sleep', 'pg_terminate_backend',
+        '@@version', '@@hostname', '@@datadir', '@@tmpdir', '@@basedir', '@@innodb_version', '@@version_compile_os',
+        'file_priv', 'root@', '0=0', '1=1', '2=2', '3=3', '4=4', '5=5', '6=6', '7=7', '8=8', '9=9'
     ]
+    # Agregar el prefijo r'' a cada palabra
+    palabras_prohibidas_o = [f"r'{palabra}'" for palabra in palabras_prohibidas]
     for palabra in palabras_prohibidas:
         # Elimina la palabra, sin importar mayúsculas/minúsculas
         texto_limpio = re.sub(palabra, '', texto_limpio, flags=re.IGNORECASE)
@@ -25,4 +27,5 @@ if __name__ == "__main__":
     entrada = input("Introduce un texto: ")
     limpio = limpiar_input(entrada)
     print(f"Texto limpio: {limpio}")
+
 
